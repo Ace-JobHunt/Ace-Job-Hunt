18d82e6be89bb48a5aed889942ef4afdea970274
Final4Head <jeffreymai3000@gmail.com>
2023-11-29 12:02:04 -0500

    testing frontend
    Co-authored-by: Nick Vanderlinden <nick.vanderlinden21@gmail.com>

--- __tests__/backendtest.js
+++ __tests__/backendtest.js
@@ -3,21 +3,23 @@ const server = 'http://localhost:3000';
 const { Job } = require('../server/models/jobModel');
 const jobController = require('../server/controllers/jobController');
 
+
+
 describe('Route integration', () => {
   const testObj = {
-    dateApplied: 2023-11-28,
+    dateApplied: new Date(),
     company: 'TestCompany',
     title: 'Software Engineer',
-    salary: 100000,
+    salary: '100000',
     status: 'Interested',
     link: 'www.google.com',
   };
 
   const patchTestObj = {
-    dateApplied: 2023-11-28,
+    dateApplied: new Date(),
     company: 'TestCompany',
-    title: 'Software Engineer',
-    salary: 100000,
+    title: 'CEO',
+    salary: '200000',
     status: 'Interested',
     link: 'www.google.com',
   };
@@ -33,83 +35,71 @@ describe('Route integration', () => {
     });  
     
     describe('POST', () => {
+      it('responds with 200 status and JSON content type', async () => {
+        const response = await request(server)
+          .post('/')
+          .send(testObj)
+          .expect(302);
+        await Job.deleteMany({ company: 'TestCompany' });
+      });
+    
+      it('database contains the new post', async () => {
+        const response = await request(server)
+          .post('/')
+          .send(testObj);
+
+        const createdJob = await Job.findOne({ company: 'TestCompany' });
+        expect(createdJob.toObject().company).toEqual(testObj.company);
+        await Job.deleteMany({ company: 'TestCompany' });
+      });
+    });
+  
+  describe('/:id', () => {
+    describe('PATCH', () => {
+        it('post in the database is updated with new status', async () => {
+          const response = await request(server)
+            .post('/')
+            .send(testObj);
+          
+          const app = await Job.findOne({company: 'TestCompany'});
+          const id = app._id.toString();
+
+          const patchResponse = await request(server)
+            .patch(`/${id}`)
+            .send(patchTestObj);
+
+          const updatedApp = await Job.findOne({ _id: id });
+          expect(updatedApp.toObject().title).toEqual(patchTestObj.title);
+
+          // NOT DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+          
+          // expect(app).toBe(123);
+          // expect(app.toObject().title).toEqual(patchTestObj.title);
+
+          await Job.deleteMany({ company: 'TestCompany' });
+        });
+      });
+    });
+
+
+    xdescribe('DELETE', () => {
       it('responds with 200 status and text/html content type', () => {
         return request(server)
           .get('/')
           .expect('Content-Type', /text\/html/)
           .expect(200);
-      });          
-      it('database contains the new post', async () => {
+      });
+      it('deletes post', async () => {
+        const app = await Job.findOne({company: 'TestCompany'});
+          const id = app._id.toString();
         const response = await request(server)
-          .post('/')
-          .send([testObj]);
-
-        Job.findOne({company: 'TestCompany'})
-          .then(response => response.json())
-          .then(data => {
-            expect(data).toBe(testObj);
-          })
-        // Job.findOneAndDelete({company: 'TestCompany'});
+          .delete(`/${id}`)
+        expect(app).toBe(123);
+          // .end((err, res) => {
+          //   // expect(res.body).toBe(testObj);
+          // });
       });
     });
-  });
-
-  describe('/:id', () => {
-    // set up beforeEach, beforeAll to create a dummy data and grab _id
-
-    describe('PATCH', () => {
-        it('responds with 200 status and text/html content type', () => {
-            return request(server)
-              .get('/')
-              .expect('Content-Type', /text\/html/)
-              .expect(200);
-          });          
-          it('post in the database is updated with new status', async () => {
-            // const postResponse = await request(server)
-            //   .post('/')
-            //   .send([testObj])
-            //   .end();
-
-              
-              const app = await Job.findOne({company: 'TestCompany'});
-              const id = app._id;
-              
-                
-
-              const patchResponse = await request(server)
-              .patch(`/${id}`)
-              .send([patchTestObj])
-              .end();
-
-              const updatedApp = await Job.findOne({company: 'TestCompany'});
-              expect(updatedApp).toEqual(patchTestObj)
-    
-            Job.findOneAndDelete({company: 'TestCompany'});
-          });
-        });
-    });
-
-
-  describe('DELETE', () => {
-    it('responds with 200 status and text/html content type', () => {
-      return request(server)
-        .get('/')
-        .expect('Content-Type', /text\/html/)
-        .expect(200);
-    });          
-    it('deletes post', async () => {
-      const app = Job.findOne({company: 'TestCompany'});
-      const id = app._id;
-      const response = await request(server)
-        .delete(`/${id}`)
-        // .expect(200);
-        // .end((err, res) => {
-        //   // expect(res.body).toBe(testObj);
-        // });
-      
-      expect(id).toBe(testObj);
-    });
-  });
 });
 
-
+});
